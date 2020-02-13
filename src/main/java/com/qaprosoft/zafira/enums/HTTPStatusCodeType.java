bbc83232da4fa433d2652043b12f7b110a5f405a
khsboris <khsboris@gmail.com>
2020-02-13 12:01:11 +0300

    add fix for deleteTestById

--- src/main/java/com/qaprosoft/zafira/enums/HTTPStatusCodeType.java
+++ src/main/java/com/qaprosoft/zafira/enums/HTTPStatusCodeType.java
@@ -3,6 +3,7 @@ package com.qaprosoft.zafira.enums;
 public enum HTTPStatusCodeType {
     OK(200),
     CREATED(201),
+    NO_CONTENT(204),
     BAD_REQUEST(400),
     UNAUTHORIZED(401),
     NOT_FOUND(404),
