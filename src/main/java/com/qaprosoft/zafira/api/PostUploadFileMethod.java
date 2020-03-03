4d1a9d2fd976dbac2f4b2373fd058ef1f420cc66
khsboris <khsboris@gmail.com>
2020-03-03 10:57:14 +0300

    added fix for UploadFIle

--- src/main/java/com/qaprosoft/zafira/api/PostUploadFileMethod.java
+++ src/main/java/com/qaprosoft/zafira/api/PostUploadFileMethod.java
@@ -1,8 +1,8 @@
 package com.qaprosoft.zafira.api;
 
-import com.qaprosoft.zafira.manager.APIContextManager;
-
 import java.io.File;
+
+import com.qaprosoft.zafira.manager.APIContextManager;
 
 public class PostUploadFileMethod extends ZafiraBaseApiMethodWithAuth {
     public PostUploadFileMethod(File uploadFile) {
@@ -10,7 +10,7 @@ public class PostUploadFileMethod extends ZafiraBaseApiMethodWithAuth {
         replaceUrlPlaceholder("base_api_url", APIContextManager.API_URL);
         setHeaders("FileType=USERS");
         setHeaders("Content-Type=multipart/form-data");
-        request.multiPart("file", uploadFile, "application/jpg");
+        request.multiPart("file", uploadFile, "image/jpeg");
     }
 }
 
