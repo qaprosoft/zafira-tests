d7830ad793a83673113889a1f573c89ee2ab60ed
Olga Babich <obabich@Olgas-Mac-mini.local>
2020-09-29 13:55:03 +0300

    added correction

--- src/main/java/com/qaprosoft/zafira/api/testRunController/v1/PostStartTestRunV1Method.java
+++ src/main/java/com/qaprosoft/zafira/api/testRunController/v1/PostStartTestRunV1Method.java
@@ -8,13 +8,12 @@ import java.util.UUID;
 
 
 public class PostStartTestRunV1Method extends ZafiraBaseApiMethodWithAuth {
-        OffsetDateTime date =OffsetDateTime.now();
         public PostStartTestRunV1Method() {
         super("api/test_run/v1/_post/rq.json", "api/test_run/v1/_post/rs.json", "api/test_run.properties");
         replaceUrlPlaceholder("base_api_url", APIContextManager.BASE_URL);
         replaceUrlPlaceholder("project", APIContextManager.PROJECT_KEY);
         addProperty("uuid", UUID.randomUUID());
-        addProperty("startedAt",date);
+        addProperty("startedAt",OffsetDateTime.now());
 
     }
 }
--- src/main/java/com/qaprosoft/zafira/api/testRunController/v1/PutFinishTestRunV1Method.java
+++ src/main/java/com/qaprosoft/zafira/api/testRunController/v1/PutFinishTestRunV1Method.java
@@ -7,12 +7,10 @@ import java.time.OffsetDateTime;
 
 
 public class PutFinishTestRunV1Method extends ZafiraBaseApiMethodWithAuth {
-        OffsetDateTime date =OffsetDateTime.now();
         public PutFinishTestRunV1Method(int ciRunId) {
         super("api/test_run/v1/_put/rq.json", null, "api/test_run.properties");
         replaceUrlPlaceholder("base_api_url", APIContextManager.BASE_URL);
         replaceUrlPlaceholder("testRunId", String.valueOf(ciRunId));
-        OffsetDateTime date =OffsetDateTime.now();
-        addProperty("endedAt", date);
+        addProperty("endedAt", OffsetDateTime.now());
         }
 }
