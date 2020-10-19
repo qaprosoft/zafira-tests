060e263bc148bb01b0b3c58edf7a3a48be30a995
Olga Babich <obabich@Olgas-Mac-mini.local>
2020-10-19 13:14:05 +0300

    added asset  test

--- src/test/java/com/qaprosoft/zafira/api/UserTest.java
+++ src/test/java/com/qaprosoft/zafira/api/UserTest.java
@@ -311,7 +311,7 @@ public class UserTest extends ZafiraAPIBaseTest {
         int userId = userV1ServiceAPIImpl.getUserId(username);
         String newEmail = INVALID_EMAIL;
         PatchUserV1Method patchUserV1Method = new PatchUserV1Method(userId, EMAIL_KEY_FOR_UPDATE, newEmail);
-        apiExecutor.expectStatus(patchUserV1Method, HTTPStatusCodeType.FORBIDDEN);
+        apiExecutor.expectStatus(patchUserV1Method, HTTPStatusCodeType.BAD_REQUEST);
         apiExecutor.callApiMethod(patchUserV1Method);
     }
 
