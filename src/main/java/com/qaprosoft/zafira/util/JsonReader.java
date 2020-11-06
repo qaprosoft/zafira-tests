a3b1dd5e04bfd66343f8c718d40994187808bda8
obabich-solvd <71430326+obabich-solvd@users.noreply.github.com>
2020-11-06 15:49:35 +0300

    added fixes in tests with tads fields (#89)
    Co-authored-by: Olga Babich <obabich@Olgas-Mac-mini.local>

---
+++ src/main/java/com/qaprosoft/zafira/util/JsonReader.java
@@ -0,0 +1,18 @@
+package com.qaprosoft.zafira.util;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonReader {
+
+    public static List<String> readList(String str)  {
+        Gson googleJson = new Gson();
+        Type listType = new TypeToken<ArrayList<String>>() {}.getType();
+        List<String> jsonObjList = googleJson.fromJson(str, listType);
+        return jsonObjList;
+    }
+}
