205bf7228d3193c416887498e35d9a1eae9e5e7e
Olga Babich <obabich@Olgas-Mac-mini.local>
2020-11-27 16:32:15 +0300

    added fixes in launchersTest

--- src/main/java/com/qaprosoft/zafira/api/jobController/PostJobMethod.java
+++ src/main/java/com/qaprosoft/zafira/api/jobController/PostJobMethod.java
@@ -8,5 +8,6 @@ public class PostJobMethod extends ZafiraBaseApiMethodWithAuth {
     public PostJobMethod() {
         super("api/job/_post/rq.json", "api/job/_post/rs.json", "api/job.properties");
         replaceUrlPlaceholder("base_api_url", APIContextManager.API_URL);
+        addProperty("automationServerId", APIContextManager.AUTHOMATION_SERVER_ID_VALUE);
     }
 }
