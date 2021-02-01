97eee772e14b920a09cba7c9e3ba020b800ec4a0
Olga Babich <obabich@Olgas-Mac-mini.local>
2021-02-01 19:05:30 +0300

    fix

--- src/main/java/com/qaprosoft/zafira/service/impl/JobServiceImpl.java
+++ src/main/java/com/qaprosoft/zafira/service/impl/JobServiceImpl.java
@@ -17,7 +17,7 @@ public class JobServiceImpl implements JobService {
     @Override
     public int create() {
         String response = apiExecutor.callApiMethod(new PostJobMethod());
-        return 1;
+        return JsonPath.from(response).getInt(JSONConstant.ID_KEY);
     }
 
 }
