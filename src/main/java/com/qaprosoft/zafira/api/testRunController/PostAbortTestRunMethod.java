6c276233a4db9b3bf2413691405f8073c7e72022
obabich <obabich@solvd.com>
2021-04-06 17:08:12 +0300

    deleted field blocker from rs and added in some rs field logicallyFailed

--- src/main/java/com/qaprosoft/zafira/api/testRunController/PostAbortTestRunMethod.java
+++ src/main/java/com/qaprosoft/zafira/api/testRunController/PostAbortTestRunMethod.java
@@ -5,7 +5,7 @@ import com.qaprosoft.zafira.manager.APIContextManager;
 
 public class PostAbortTestRunMethod extends ZafiraBaseApiMethodWithAuth {
     public PostAbortTestRunMethod(int testRunId, String ciRunId) {
-        super("api/test_run/_post/rq_to_abort.json", "", "api/test_run.properties");
+        super("api/test_run/_post/rq_to_abort.json", "api/test_run/_post/rs_to_abort.json", "api/test_run.properties");
         replaceUrlPlaceholder("base_api_url", APIContextManager.API_URL);
         replaceUrlPlaceholder("id", String.valueOf(testRunId));
         replaceUrlPlaceholder("ciRunId", ciRunId);
