36d9681c5fb8e105554271abf44b33f0b5ee7e4c
obabich <obabich@solvd.com>
2021-04-06 13:44:12 +0300

    added tests on patch to rate user feedback and added PatchProjectV1Method

---
+++ src/main/java/com/qaprosoft/zafira/api/projectsV1/PatchProjectV1Method.java
@@ -0,0 +1,20 @@
+package com.qaprosoft.zafira.api.projectsV1;
+
+import com.qaprosoft.carina.core.foundation.api.annotation.Endpoint;
+import com.qaprosoft.carina.core.foundation.api.annotation.RequestTemplatePath;
+import com.qaprosoft.carina.core.foundation.api.annotation.ResponseTemplatePath;
+import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
+import com.qaprosoft.zafira.api.ZafiraBaseApiMethodWithAuth;
+import com.qaprosoft.zafira.manager.APIContextManager;
+
+@RequestTemplatePath(path = "api/projectsV1/_patch/rq.json")
+@ResponseTemplatePath(path = "api/projectsV1/_patch/rs.json")
+@Endpoint(url = "${projects_url}/v1/projects/${key}", methodType = HttpMethodType.PUT)
+public class PatchProjectV1Method extends ZafiraBaseApiMethodWithAuth {
+    public PatchProjectV1Method(String key, int leadId) {
+        replaceUrlPlaceholder("projects_url", APIContextManager.PROJECT_SERVICE_URL);
+        replaceUrlPlaceholder("key", key);
+        setProperties("api/projectV1.properties");
+        addProperty("leadId", leadId);
+    }
+}
