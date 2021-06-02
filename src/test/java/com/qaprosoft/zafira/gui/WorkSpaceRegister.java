6e42632a142d9042a534a02db9c8356af2e219b4
DimaGrinevich <78387779+DimaGrinevich@users.noreply.github.com>
2021-06-02 16:42:38 +0300

    Update WorkSpaceRegister.java

--- src/test/java/com/qaprosoft/zafira/gui/WorkSpaceRegister.java
+++ src/test/java/com/qaprosoft/zafira/gui/WorkSpaceRegister.java
@@ -13,7 +13,6 @@ public class WorkSpaceRegister extends BaseWorkSpaceRegister {
         landingPage.closeCookiesNotification();
         LandingRegistrationComp registrationComp = landingPage.getRegistrationComponent();
         registrationComp.createFreeWorkspace(orgName, ownerName,tenantEmail );
-        landingPage.clickOnCaptcha();
-        int i = 22+2;
+        //landingPage.clickOnCaptcha();
     }
 }
