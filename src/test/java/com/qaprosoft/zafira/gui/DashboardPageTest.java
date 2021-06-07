8e3da59d2e450ad3fa2b34235e6cc6432dce60ee
obabich <obabich@solvd.com>
2021-06-07 17:48:12 +0300

    name fix

--- src/test/java/com/qaprosoft/zafira/gui/DashboardPageTest.java
+++ src/test/java/com/qaprosoft/zafira/gui/DashboardPageTest.java
@@ -2,6 +2,7 @@ package com.qaprosoft.zafira.gui;
 
 import com.qaprosoft.zafira.gui.desktop.component.Dashboard;
 import com.qaprosoft.zafira.gui.desktop.page.tenant.DashboardsPage;
+import org.apache.commons.lang.RandomStringUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,7 +13,7 @@ public class DashboardPageTest extends SignIn {
 
         DashboardsPage dashboardsPage = navigationMenu.toDashboardPage();
         Dashboard dashboard = dashboardsPage.addDashboard();
-        dashboard.createDashboard("new dashboard");
+        dashboard.createDashboard("Dashboard_Name_".concat(RandomStringUtils.randomAlphabetic(6)));
         Assert.assertTrue(dashboardsPage.isPageOpened(), "DashboardPage is not opened!");
     }
 }
