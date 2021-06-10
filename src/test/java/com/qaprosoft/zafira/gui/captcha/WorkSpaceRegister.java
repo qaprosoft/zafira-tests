03e0d41f51ffd37f735bc7f9b7bc4a441b4c249a
Dima Hrynevich <dhrynevich@solvd.com>
2021-06-10 10:53:59 +0300

    added assertion for workspace creation step

--- src/test/java/com/qaprosoft/zafira/gui/captcha/WorkSpaceRegister.java
+++ src/test/java/com/qaprosoft/zafira/gui/captcha/WorkSpaceRegister.java
@@ -2,17 +2,19 @@ package com.qaprosoft.zafira.gui.captcha;
 
 import com.qaprosoft.zafira.gui.desktop.component.LandingRegistrationComp;
 import com.qaprosoft.zafira.gui.desktop.page.LandingPage;
+import org.junit.Assert;
 import org.testng.annotations.Test;
 
 public class WorkSpaceRegister extends BaseWorkSpaceRegister {
 
     @Test
-    public void createNewFreeWorkspace(){
+    public void createNewFreeWorkspace() {
         LandingPage landingPage = new LandingPage(getDriver());
         landingPage.open();
         landingPage.closeCookiesNotification();
         LandingRegistrationComp registrationComp = landingPage.getRegistrationComponent();
-        registrationComp.createFreeWorkspace(orgName, ownerName,tenantEmail );
-        landingPage.clickOnCaptcha();
+        Assert.assertTrue("Registration component isn't present", registrationComp.isUIObjectPresent());
+        registrationComp.createFreeWorkspace(orgName, ownerName, tenantEmail);
+       // landingPage.clickOnCaptcha();
     }
 }
