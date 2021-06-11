58ab5e4ab47e9831e1f6d193c98b29db5dbd532f
Dima Hrynevich <dhrynevich@solvd.com>
2021-06-11 10:17:56 +0300

    added minor fix

--- src/test/java/com/qaprosoft/zafira/gui/SignIn.java
+++ src/test/java/com/qaprosoft/zafira/gui/SignIn.java
@@ -12,7 +12,6 @@ import org.testng.annotations.BeforeTest;
 
 public class SignIn extends AbstractTest {
 
-    private String signInUrl;
     protected NavigationMenu navigationMenu;
 
     @BeforeTest
@@ -20,11 +19,11 @@ public class SignIn extends AbstractTest {
         StringBuilder urlBuilder = new StringBuilder(Configuration.get(Configuration.Parameter.URL));
         urlBuilder.insert(urlBuilder.indexOf("://") + 3, R.TESTDATA.get(WebConstant.TENANT_NAME) + ".");
         urlBuilder.append("signin");
-        signInUrl = urlBuilder.toString();
+        String signInUrl = urlBuilder.toString();
 
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.openURL(signInUrl);
-        Assert.assertTrue(!loginPage.isSubmitButtonActive(), "Submit button should be inactive" +
+        Assert.assertFalse(loginPage.isSubmitButtonActive(), "Submit button should be inactive" +
                 " because of empty input fields");
         TestRunsPage testRunsPage = loginPage.login(R.TESTDATA.get(WebConstant.USER_LOGIN), R.TESTDATA.get(WebConstant.USER_PASSWORD));
         testRunsPage.assertPageOpened();
